f8a68b839001d06b81b17f17b1239a501c5cc56f
shreyashukla2 <shukla.12@iitj.ac.in>
2024-04-28 08:44:35 +0530

    updated scripts for inference on custom dataset

--- SSL_Anti-spoofing/evaluate_2021_DF.py
+++ SSL_Anti-spoofing/evaluate_2021_DF.py
@@ -16,6 +16,7 @@ import numpy as np
 import pandas
 import eval_metrics_DF as em
 from glob import glob
+import json
 
 if len(sys.argv) != 4:
     print("CHECK: invalid input arguments. Please read the instruction below:")
@@ -26,13 +27,18 @@ submit_file = sys.argv[1]
 truth_dir = sys.argv[2]
 phase = sys.argv[3]
 
-cm_key_file = os.path.join(truth_dir, 'CM/trial_metadata.txt')
+cm_key_file = os.path.join(truth_dir, 'CM/CustomDataset_metadata.txt')
 
 
 def eval_to_score_file(score_file, cm_key_file):
     
-    cm_data = pandas.read_csv(cm_key_file, sep=' ', header=None)
-    submission_scores = pandas.read_csv(score_file, sep=' ', header=None, skipinitialspace=True)
+    cm_data = pandas.read_csv(cm_key_file, sep='- ', header=None)
+    cm_data[1] = cm_data[1].str.strip()
+    cm_data[4] = cm_data[4].str.strip()
+    
+    submission_scores = pandas.read_csv(score_file, sep=' = ', header=None, skipinitialspace=True)
+    submission_scores[0] = pandas.DataFrame([file.split('/')[-1] for file in list(submission_scores[0])])[0]
+    
     if len(submission_scores) != len(cm_data):
         print('CHECK: submission has %d of %d expected trials.' % (len(submission_scores), len(cm_data)))
         exit(1)
@@ -40,13 +46,21 @@ def eval_to_score_file(score_file, cm_key_file):
     if len(submission_scores.columns) > 2:
         print('CHECK: submission has more columns (%d) than expected (2). Check for leading/ending blank spaces.' % len(submission_scores.columns))
         exit(1)
-            
-    cm_scores = submission_scores.merge(cm_data[cm_data[7] == phase], left_on=0, right_on=1, how='inner')  # check here for progress vs eval set
-    bona_cm = cm_scores[cm_scores[5] == 'bonafide']['1_x'].values
-    spoof_cm = cm_scores[cm_scores[5] == 'spoof']['1_x'].values
+
+    cm_scores = submission_scores.merge(cm_data[cm_data[5] == phase], left_on=0, right_on=1, how='inner')  # check here for progress vs eval set
+    bona_cm = cm_scores[cm_scores[4] == 'bonafide']['1_x'].values
+    spoof_cm = cm_scores[cm_scores[4] == 'spoof']['1_x'].values
+
     eer_cm = em.compute_eer(bona_cm, spoof_cm)[0]
     out_data = "eer: %.2f\n" % (100*eer_cm)
     print(out_data)
+    dic = {
+        'bona_cm': str(bona_cm),
+        'spoof_cm': str(spoof_cm),
+        'EER': 100*eer_cm
+    }
+    with open('eer_auc_pretrained_custom_dataset.json', 'w') as f:
+        json.dump(dic, f, indent=2)
     return eer_cm
 
 if __name__ == "__main__":
--- SSL_Anti-spoofing/main_SSL_DF.py
+++ SSL_Anti-spoofing/main_SSL_DF.py
@@ -66,7 +66,7 @@ def produce_evaluation_file(dataset, model, device, save_path):
         
         with open(save_path, 'a+') as fh:
             for f, cm in zip(fname_list,score_list):
-                fh.write('{} {}\n'.format(f, cm))
+                fh.write('{} = {}\n'.format(f, cm))
         fh.close()   
     print('Scores saved to {}'.format(save_path))
 
